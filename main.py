b2fab882f6e98feeadec3f52b68770495f7ed798
hkgchok <hkgoldenback@gmail.com>
2021-07-18 01:20:48 +0800

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 #Logger Setup
 logger = logging.getLogger(__name__)
 
-TOKEN = "YOUR_TOKEN_HERE"
+TOKEN = "1935732399:AAHdEsJsBzEHS2k3dYDvPeJ0rhc41E9WkPo"
 
 def download(bot, update):
     message = update.effective_message
